da6517c42ac7be5307be6beb80c76c74e34790ed
wuchuixu <364191418@qq.com>
2018-09-17 23:08:01 +0800

    score

---
+++ pages/subs/score/index.js
@@ -0,0 +1,81 @@
+// const userModel = require('../../../../models/user/index.js');
+const app = getApp();
+Page({ 
+  data: {
+    productInfos:{
+      date: '5',
+      timeRange: '9:00 ~ 10:00',
+      title: 'abc',
+      imageUrl: '',
+      name: '123',
+      star: 5,
+      times: [
+        {
+          time: '9:00',
+          content: '123123'
+        },
+        {
+          time: '9:30',
+          content: '123123'
+        },
+        {
+          time: '10:00',
+          content: '123123'
+        }
+      ]
+    },
+    form: {
+      personStar: 0,
+      productStar: 0
+    },
+    personTexts: [
+      {
+        text: '准时到达',
+        checked: false
+      },
+      {
+        text: '服务态度好',
+        checked: false
+      },
+      {
+        text: '礼貌',
+        checked: false
+      },
+      {
+        text: '善于沟通',
+        checked: false
+      },
+      {
+        text: '穿着得体',
+        checked: false
+      }
+    ],
+    productTexts: [
+      {
+        text: '效果明显',
+        checked: false
+      },
+      {
+        text: '无副作用',
+        checked: false
+      },
+      {
+        text: '物超所值',
+        checked: false
+      },
+      {
+        text: '值得推荐',
+        checked: false
+      }
+    ],
+    images: []
+  },
+  onLoad: function () {
+    
+  },
+  onShow: function () {
+    
+  }
+})
+
+
