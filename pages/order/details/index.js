908a1e5c43494a2a2a9435ba81f29cc774fbf8c0
yx570 <16510404@qq.com>
2018-10-11 16:03:35 +0800

    icon

--- pages/order/details/index.js
+++ pages/order/details/index.js
@@ -3,13 +3,20 @@ const app = getApp();
 Page({
   // ...app.loadcartlist,
   data: {
+    statusFormat: {
+      1: "已服务",
+      2: "技师已出发",
+      3: "服务中",
+      4: "待服务"
+    },
     orderInfos: {
       number: '201809192020',
       imageUrl: '/static/images/demo/b1.jpg',
-      title: '产品名称大标题',
+      title: '产品名称大标题产品名称大标题产品名称大标题产品名称大标题',
       total: 3,
       status: 1,
-      price: 1000
+      perPrice: 982,
+      price: 1000,
     },
     list: [
       {
@@ -17,17 +24,19 @@ Page({
         title: '产品名称大标题',
         name: '王小丫',
         star: 5,
-        date: '5月1日',
-        timeRange: '9:00 ~ 10:00',
+        date: '2018年5月1日',
+        star: 3,
+        timeRange: '9:00-10:00',
         status: 1
       },
       {
         imageUrl: '/static/images/demo/b1.jpg',
-        title: '产品名称大标题',
+        title: '产品名称大标题产品名称大标题产品名称大标题产品名称大标题产品名称大标题产品名称大标题',
         name: '王小丫',
         star: 5,
-        date: '5月1日',
-        timeRange: '9:00 ~ 10:00',
+        date: '2018年5月1日',
+        star: 3,
+        timeRange: '9:00-10:00',
         status: 2
       },
       {
@@ -35,9 +44,20 @@ Page({
         title: '产品名称大标题',
         name: '王小丫',
         star: 5,
-        date: '5月1日',
-        timeRange: '9:00 ~ 10:00',
-        status: 0
+        star: 3,
+        date: '2018年5月1日',
+        timeRange: '9:00-10:00',
+        status: 3
+      },
+      {
+        imageUrl: '/static/images/demo/b1.jpg',
+        title: '产品名称大标题',
+        name: '王小丫',
+        star: 3,
+        star: 5,
+        date: '2018年5月1日',
+        timeRange: '9:00-10:00',
+        status: 4
       }
     ]
   },
